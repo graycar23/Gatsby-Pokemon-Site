d721b6c156dbc3dee585c421b2d63f6d287f3e7e
graycar23 <graycar23@gmail.com>
2022-04-11 21:52:54 -0400

    Broken Browser

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const axios = require("axios")
+const { default: pokemon } = require("./src/template/pokemon")
 const get = endpoint => axios.get(`https://pokeapi.co/api/v2${endpoint}`)
 const getPokemonData = names =>
   Promise.all(
@@ -18,11 +19,23 @@ exports.createPages = async ({ actions: { createPage } }) => {
   })
   
   for (let i = 0; i < allPokemon.length; i++){
-    pokemon = allPokemon[i]
+    pokemon = [allPokemon[i]]
+
+    if (i-1 >= 0){
+      pokemon.push(allPokemon[i-1])
+    }
+
+    if (i+1 < allPokemon.length){
+      pokemon.push(allPokemon[i+1])
+    }
+
     createPage({
-      path: `/pokemon/${pokemon.name}`,
+      path: `/pokemon/${pokemon[0].name}`,
       component: require.resolve("/home/graycar23/my-first-gastby-site-main/src/template/pokemon.js"),
       context: { pokemon },
     })
+    
+    pokemon.length = 0
+
   }
 }
--- src/template/pokemon.js
+++ src/template/pokemon.js
@@ -4,17 +4,17 @@ export default function pokemon({ pageContext: { pokemon } }) {
   return (
     <div>
       <ul>
-          <li key={pokemon.id}>
-            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-            <h2>Name: {pokemon.name}</h2>
-            <h3>Main Type: {pokemon.types[0].type.name}</h3>
+          <li key={pokemon[0].id}>
+            <img src={pokemon[0].sprites.front_default} alt={pokemon[0].name} />
+            <h2>Name: {pokemon[0].name}</h2>
+            <h3>Main Type: {pokemon[0].types[0].type.name}</h3>
             <h3>Stats: </h3>
-            <h4>{pokemon.stats[0].stat.name} - {pokemon.stats[0].base_stat}</h4>
-            <h4>{pokemon.stats[1].stat.name} - {pokemon.stats[1].base_stat}</h4>
-            <h4>{pokemon.stats[2].stat.name} - {pokemon.stats[2].base_stat}</h4>
-            <h4>{pokemon.stats[3].stat.name} - {pokemon.stats[3].base_stat}</h4>
-            <h4>{pokemon.stats[4].stat.name} - {pokemon.stats[4].base_stat}</h4>
-            <h4>{pokemon.stats[5].stat.name} - {pokemon.stats[5].base_stat}</h4>
+            <h4>{pokemon[0].stats[0].stat.name} - {pokemon[0].stats[0].base_stat}</h4>
+            <h4>{pokemon[0].stats[1].stat.name} - {pokemon[0].stats[1].base_stat}</h4>
+            <h4>{pokemon[0].stats[2].stat.name} - {pokemon[0].stats[2].base_stat}</h4>
+            <h4>{pokemon[0].stats[3].stat.name} - {pokemon[0].stats[3].base_stat}</h4>
+            <h4>{pokemon[0].stats[4].stat.name} - {pokemon[0].stats[4].base_stat}</h4>
+            <h4>{pokemon[0].stats[5].stat.name} - {pokemon[0].stats[5].base_stat}</h4>
             <p></p>
           </li>
       </ul>
