8b4a13d4e46b961baf291ec6931dbbe276fb43eb
graycar23 <graycar23@gmail.com>
2022-04-18 13:06:00 -0400

    Broken Pie on Pokemon.js

--- src/template/pokemon.js
+++ src/template/pokemon.js
@@ -1,6 +1,15 @@
 import React from "react"
+import * as d3 from "d3"
+import Pie from '../components/pie'
 export default function pokemon({ pageContext: { pokemon } }) {
   console.log(pokemon)
+
+  const pieDataValues = (value, length = 6) =>
+  d3.range(length).map((item, index) => ({
+    label: 'Label'+ index,
+    value: value === null || value === undefined ? Math.random() * 100 : value
+  }))
+
   return (
     <div>
       <ul>
@@ -16,6 +25,16 @@ export default function pokemon({ pageContext: { pokemon } }) {
             <h4>{pokemon.stats[4].stat.name} - {pokemon.stats[4].base_stat}</h4>
             <h4>{pokemon.stats[5].stat.name} - {pokemon.stats[5].base_stat}</h4>
             <p></p>
+            <>
+            <Pie
+              data={pieDataValues}
+              width={400}
+              height={400}
+              innerRadius={100}
+              outerRadius={200}
+              cornerRadius={15}
+            />
+            </>
           </li>
       </ul>
     </div>
